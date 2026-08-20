43766e17d0b01ea3732f63ba0b0b66cc5a2b6d35
Brandon <havens-useless02@icloud.com>
2026-08-20 09:31:42 -0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+
+        Dictionary<int,int> map = new Dictionary<int,int>();
+
+        for(int i = 0; i < nums.Length; i++) {
+
+            if(map.ContainsKey(target - nums[i])) {
+                return new int[2] { map[target-nums[i]], i };
+            }
+
+            if(!map.ContainsKey(nums[i])) {
+                map.Add(nums[i], i);
+            }
+        }
+
+        return new int[0];
+
+    }
+}
